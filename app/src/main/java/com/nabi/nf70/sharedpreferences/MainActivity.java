876215eef0678ebe122768ff07467ef145ebb2da
nf70 <love1023602>
2015-05-28 17:07:43 +0800

    修改

--- app/src/main/java/com/nabi/nf70/sharedpreferences/MainActivity.java
+++ app/src/main/java/com/nabi/nf70/sharedpreferences/MainActivity.java
@@ -3,71 +3,61 @@ package com.nabi.nf70.sharedpreferences;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
-    private EditText nameEditText;
+    private EditText nameField;
+    private EditText phoneField;
+    private EditText genderField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        nameEditText = (EditText) findViewById(R.id.nameEditText);
+        nameField = (EditText) findViewById(R.id.nameField);
+        phoneField = (EditText) findViewById(R.id.phoneField);
+        genderField = (EditText) findViewById(R.id.genderField);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    //按下Save按鈕
+    //按下儲存按鈕
     public void btnSave_Click(View view) {
         //取得SharedPreference設定("Preference"為設定檔的名稱)
         SharedPreferences settings = getSharedPreferences("Preference", 0);
-        //置入name屬性的字串
-        settings.edit().putString("name", nameEditText.getText().toString()).commit();
+        //置入字串
+        settings.edit()
+                .putString("name", nameField.getText().toString())
+                .putString("phone",phoneField.getText().toString())
+                .putString("gender",genderField.getText().toString())
+                .commit();
+        Toast.makeText(MainActivity.this, "儲存成功", Toast.LENGTH_LONG).show();
     }
 
-    //按下Restore按鈕
-    public void btnRestor_Click(View view) {
-        //取得SharedPreference設定("Preference"為設定檔的名稱)
-        SharedPreferences settings = getSharedPreferences("Preference", 0);
-        //取出name屬性的字串
-        String name = settings.getString("name", "");
-        nameEditText.setText(name);
-    }
-
+    //按下清除按鈕
     public void btnClear_Click(View view) {
         //取得SharedPreference設定("Preference"為設定檔的名稱)
-        SharedPreferences settings = getSharedPreferences("Preference",0);
-        //將name資料進行清空動作
+        SharedPreferences settings = getSharedPreferences("Preference", 0);
+        //資料進行清空動作
         settings.edit()
                 .clear()
                 .commit();
-        Toast.makeText(MainActivity.this,"清除完成", Toast.LENGTH_LONG).show();
+        Toast.makeText(MainActivity.this, "清除完成", Toast.LENGTH_LONG).show();
+    }
+
+    //按下讀取按鈕
+    public void btnRead_Click(View view) {
+        //取得SharedPreference設定("Preference"為設定檔的名稱)
+        SharedPreferences settings = getSharedPreferences("Preference", 0);
+        //取出字串
+        String name = settings.getString("name", "");
+        String phone = settings.getString("phone","");
+        String gender=settings.getString("gender","");
+        nameField.setText(name);
+        phoneField.setText(phone);
+        genderField.setText(gender);
+        Toast.makeText(MainActivity.this,"讀取完成",Toast.LENGTH_LONG).show();
     }
 }
